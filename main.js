91eadcad009e51f0ba859836ef655ee119aa0e1f
Aswin Prasanth <aswinprasanth.mec@gmail.com>
2021-09-29 14:17:02 +0530

    partially completed

--- main.js
+++ main.js
@@ -1,35 +1,144 @@
 $('.responsive').slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+       
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.responsive2').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.responsive3').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+const hamo = document.getElementById("open");
+const hamc = document.getElementById("close");
+const hammenu = document.getElementById("hamburger");
+hamc.style.display = 'none';
+hammenu.style.display = 'block';
+let i=0;
+function hamfunc(){
+  if(i===0){
+    i=1;
+  hammenu.style.display = 'block';
+  hamo.style.display = 'none';
+  hamc.style.display = 'block';}
+  else{
+    i=0;
+    hammenu.style.display = 'none';
+    hamo.style.display = 'block';
+    hamc.style.display = 'none';
+  }
+}
+var acc = document.getElementsByClassName("accordion");
+// var hide = document.getElementsByClassName("hide");
+var j;
+
+for (j = 0; i < acc.length; i++) {
+  acc[j].addEventListener("click", function() {
+    this.classList.toggle("active");
+    // hide[j].style.display='none';
+    var panel = this.nextElementSibling;
+    console.log(panel);
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      // hide[j].style.display='inline';
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
   });
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,41 +4,13 @@ module.exports = {
   important: true,
   
   theme: {
-    screens: {
-      'tablet': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'laptop': '1440px',
-      // => @media (min-width: 1024px) { ... }
-
-      'desktop': '1280px',
-      // => @media (min-width: 1280px) { ... }
-    },
+   
     container :{
       center : true,
     },
-    // backgroundColor: theme => ({
-    //   ...theme('colors'),
-    //   'primary': '#3490dc',
-    //   'secondary': '#ffed4a',
-    //   'danger': '#e3342f',
-      
-    //  }),
-    // colors:{
-    //   prime:{
-    //     dark: '#263238',
-    //     light:'#445A64',
-        
-        
-    //   },
+ 
      
-    // },
     extend: {
-      lineHeight: {
-        
-        '12': '3.5rem',
-       },
-       
       colors:{
         'first':'#f3ead6',
         'second':'#E7E6FF',
@@ -49,11 +21,10 @@ module.exports = {
         'BGC2': '#F7F9FF',
         'newp': '#EE7892',
         'footer':'#282828',
-        
-
-        
-
       },
+      height: {
+        ham: '92vh',
+       },
       width: {
         '1/12': '9% !important',
        },
@@ -61,9 +32,7 @@ module.exports = {
     },
     backgroundImage: {
       'center-pattern': "url('../assets/coffe.svg')",
-      
      },
-     
   },
   variants: {
     extend: {},
